9f21857e9e6e41d351f694755be713bb73dfea2a
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-03-04 16:55:49 +0700

    Update snk.js

--- plugins/snk.js
+++ plugins/snk.js
@@ -4,33 +4,32 @@ let handler = async(m, { conn }) => {
 Kebijakan Privasi, Syarat Ketentuan dan Peraturan Riel Bot
 
 Kebijakan Privasi Riel Bot
-1. Rielbot tidak akan merekam data riwayat chat user.
-2. Rielbot tidak akan menyebarkan nomor users.
-3. Rielbot tidak akan menyimpan media yang dikirimkan oleh users.
-4. Rielbot tidak akan menyalah gunakan data data users.
-5. Owner Rielbot berhak melihat data riwayat chat users.
-6. Owner Rielbot berhak melihat status users.
-7. Owner Rielbot dapat melihat riwayat chat, dan media yang dikirimkan users.
+1. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ tidak akan merekam data riwayat chat user.
+2. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ tidak akan menyebarkan nomor users.
+3. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ tidak akan menyimpan media yang dikirimkan oleh users.
+4. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ tidak akan menyalah gunakan data data users.
+5. Owner  berhak melihat data riwayat chat users.
+6. Owner  berhak melihat status users.
+7. Owner  dapat melihat riwayat chat, dan media yang dikirimkan users.
 
-Peraturan Riel Bot
+Peraturan ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙
 1. Users dilarang menelpon maupun memvideo call nomor bot.
 2. Users dilarang mengirimkan berbagai bug, virtex, dll ke nomor bot.
 3. Users diharap tidak melakukan spam dalam penggunaan bot.
 4. Users dilarang menambahkan nomor bot secara illegal, untuk menambahkan silahkan hubungi owner.
 5. Users diharap untuk tidak menyalah gunakan fitur fitur bot.
 
-Syarat Ketentuan Bot Riel
+Syarat Ketentuan ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙
 1. Bot akan keluar dari group apabila sudah waktunya keluar.
-2. RielXzy dapan mem-ban users secara sepihak terlepas dari users salah atau tidak.
-3. RielXzy *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
-4. RielXzy akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
-5. RielXzy bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
+2. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ dapan mem-ban users secara sepihak terlepas dari users salah atau tidak.
+3. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
+4. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
+5. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
 
-- Riel(owner)
-- RielxBot(bot)
-- Lan Official (pembantu)
+- Rozi(owner)
+- ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙
 
-Peraturan: 1 Februaru 2022
+Peraturan: 4 Maret 2022
 `.trim()
     conn.send2ButtonLoc(m.chat, await(await fetch(image)).buffer(), teks, watermark, 'Menu', '.menu', 'Owner', '.owner', m)
 }
