b5f79b49d388567a709aa9f89cc5f49b8b11cff5
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-03-04 15:42:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ global.eror = '「❗」Server LAGI ERROR MEK!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '© ⳹ ❋ཻུ۪۪⸙zifabotz⳹ ❋ཻུ۪۪⸙' //change the watermark 
 global.image = 'https://telegra.ph/file/052ca138a613c8859756f.jpg' //change the image
-global.thumbfoto = 'https://telegra.ph/file/6e75d7b8bec8fa992e69b.jpg'
+global.thumbfoto = 'https://telegra.ph/file/98fecb8b6378591172c05.jpg'
 
 // image
 global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
